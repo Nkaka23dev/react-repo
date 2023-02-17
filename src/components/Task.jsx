085b5bf7297598707adea14434a1ebf616bc00c0
Nkaka23dev <nkakaeric96@gmail.com>
2023-02-17 12:42:00 +0200

    finished todo app

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
 import edit from "../assets/svgs/edit.svg";
 import remo from "../assets/svgs/remo.svg";
 
 export default function Task(props) {
   return (
     <div className="px-5 flex justify-between">
-        <h1 className="text-gray-700 text-sm">{props.taskInput}</h1>
-        <div className="flex gap-2">
-          <img  className="cursor-pointer" src={edit} alt="" />
-          <img onClick={() => props.deleteUser(props.id)} className="cursor-pointer" src={remo} alt="" />
-        </div>
+      <h1 className="text-gray-700 text-sm">{props.taskInput}</h1>
+      <div className="flex gap-2">
+        <img
+          onClick={() => props.edit(props.todo)}
+          className="cursor-pointer"
+          src={edit}
+          alt=""
+        />
+        <img
+          onClick={() => props.deleteUser(props.id)}
+          className="cursor-pointer"
+          src={remo}
+          alt=""
+        />
       </div>
-  )
+    </div>
+  );
 }
-
-
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Task from "./Task";
 
 export default function Todo() {
   const [taskInput, settaskInput] = useState("");
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState(null);
 
   const getInput = (event) => {
     settaskInput(event.target.value);
   };
   const deleteUser = (id) => {
-   setTaskList( taskList.filter((value) => value.id !== id))
-   console.log(taskList)
+    setTaskList(taskList.filter((value) => value.id !== id));
+    console.log(taskList);
   };
   const addTask = () => {
     const task = {
@@ -18,6 +18,39 @@ export default function Todo() {
       name: taskInput,
     };
     setTaskList([...taskList, task]);
+    settaskInput("");
+  };
+  const editTask = (id, value) => {
+    const newTast = taskList.map((task) =>
+      task.id === id ? { ...task, name: value } : task
+    );
+    setTaskList(newTast);
+    settaskInput("");
+    settodoToEdit(null);
+  };
+  const [todoToEdit, settodoToEdit] = useState();
+
+  useEffect(() => {
+    const todos = localStorage.getItem("todos");
+    if (todos) {
+      setTaskList(JSON.parse(todos) || []);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (taskList) {
+      localStorage.setItem("todos", JSON.stringify(taskList));
+    }
+  }, [taskList]);
+
+  useEffect(() => {
+    if (todoToEdit) {
+      settaskInput(todoToEdit.name);
+    }
+  }, [todoToEdit]);
+
+  const handleEdit = (todo) => {
+    settodoToEdit(todo);
   };
   return (
     <section className="px-3 mt-40">
@@ -25,30 +58,44 @@ export default function Todo() {
         <h1 className="mx-auto text-2xl tracking-wide text-gray-600 font-bold">
           Grocery Bud
         </h1>
-        <div className="relative">
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (todoToEdit) {
+              editTask(todoToEdit.id, taskInput);
+            } else {
+              addTask();
+            }
+          }}
+          className="relative"
+        >
           <input
             className="w-full bg-[#f1f5f8] px-5 py-1 rounded-lg placeholder-gray-400"
             type="text"
+            value={taskInput}
             onChange={getInput}
             placeholder="e.g. eggs"
           />
-          <button
-            onClick={addTask}
-            className="bg-[#a5d5f8] duration-300 font-semibold hover:text-white hover:bg-[#48a8ec] px-4 py-1 rounded-r-lg absolute right-0"
-          >
+          <button className="bg-[#a5d5f8] duration-300 font-semibold hover:text-white hover:bg-[#48a8ec] px-4 py-1 rounded-r-lg absolute right-0">
             submit
           </button>
-        </div>
-        {taskList.map((value, index) => {
-          return (
-            <Task
-              key={index}
-              id={value.id}
-              deleteUser={deleteUser}
-              taskInput={value.name}
-            />
-          );
-        })}
+        </form>
+        {taskList &&
+          taskList.map((value, index) => {
+            return (
+              <Task
+                edit={handleEdit}
+                key={index}
+                id={value.id}
+                deleteUser={deleteUser}
+                taskInput={value.name}
+                todo={value}
+              />
+            );
+          })}
+
+        {!taskList?.length && <div>No todos found</div>}
       </div>
     </section>
   );
