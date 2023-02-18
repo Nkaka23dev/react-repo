c3ff38738be84ea41eb78141e8bd89d2c9f72411
Nkaka23dev <nkakaeric96@gmail.com>
2023-02-18 10:23:35 +0200

    feat: added menu

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div className="space-x-5 max-w-full mx-auto text-center py-3 bg-blue-600 text-white">
+      <Link className="text-xl " to="/click">
+        Click Button
+      </Link>
+      <Link className="text-xl " to="/todo">
+        Todo
+      </Link>
+      <Link className="text-xl " to="/">
+        Home
+      </Link>
+      <Link className="text-xl " to="/todo">
+        Todo
+      </Link>
+      <Link className="text-xl " to="/">
+        Home
+      </Link>
+      <Link className="text-xl " to="/todo">
+        Todo
+      </Link>
+      <Link className="text-xl " to="/">
+        Home
+      </Link>
+      <Link className="text-xl " to="/todo">
+        Todo
+      </Link>
+      <Link className="text-xl " to="/todo">
+        Todo
+      </Link>
+      <Link className="text-xl " to="/">
+        Home
+      </Link>
+      <Link className="text-xl " to="/todo">
+        Todo
+      </Link>
+      <Link className="text-xl " to="/">
+        Home
+      </Link>
+      <Link className="text-xl " to="/todo">
+        Todo
+      </Link>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section className="h-screen grid">
+      <div className="m-auto text-center text-3xl">
+        Welcome to My react beginners Projects!!
+      </div>
+    </section>
+  );
+}
