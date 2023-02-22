1e3d745af30fcbbee7d2e2c00b7e22c4124346bb
Nkaka23dev <nkakaeric96@gmail.com>
2023-02-22 14:39:07 +0200

    fix: destructuring props in Siderbar components of notes app

--- src/components/notes/Sidebar.jsx
+++ src/components/notes/Sidebar.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-export default function Sidebar(props) {
-  const noteElements = props.notes.map((note, index) => (
+export default function Sidebar({notes,currentNote,setCurrentNoteId,newNote,deleteNote}) {
+  const noteElements = notes.map((note, index) => (
     <div key={note.id}>
       <div
         className={`overflow-hidden w-full cursor-pointer flex justify-between items-center py-3 group ${
-          note.id === props.currentNote.id ? "bg-slate-600" : ""
+          note.id === currentNote.id ? "bg-slate-600" : ""
         }`}
-        onClick={() => props.setCurrentNoteId(note.id)}
+        onClick={() => setCurrentNoteId(note.id)}
       >
         <h4
           className={` text-sm font-normal whitespace-nowrap overflow-hidden text-ellipsis leading-5 pl-2
-          ${note.id === props.currentNote.id ? "text-white" : "text-slate-600"}
+          ${note.id === currentNote.id ? "text-white" : "text-slate-600"}
           `}
         >
           {note.body.split("\n")[0]}
         </h4>
-        {note.id === props.currentNote.id && (
+        {note.id === currentNote.id && (
           <button
-            onClick={(event) => props.deleteNote(event, note.id)}
+            onClick={(event) => deleteNote(event, note.id)}
             className="bg-none border-none text-red-700 mr-4 hidden group-hover:block"
           >
             <i className="gg-trash trash-icon"></i>
@@ -34,7 +34,7 @@ export default function Sidebar(props) {
         <h3 className="text-2xl">Notes</h3>
         <button
           className="cursor-pointer bg-slate-600 border-none text-white rounded h-8 w-8"
-          onClick={props.newNote}
+          onClick={newNote}
         >
           +
         </button>
