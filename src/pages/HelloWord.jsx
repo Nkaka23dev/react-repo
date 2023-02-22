9406cb5c6603d8456f5512144099fed3f81ecddc
Nkaka23dev <nkakaeric96@gmail.com>
2023-02-22 14:21:03 +0200

    fix: apply requested change on helloworld project

--- src/pages/HelloWord.jsx
+++ src/pages/HelloWord.jsx
@@ -5,7 +5,7 @@ export default function HelloWord() {
     <section className="h-screen grid">
       <div className="bg-yellow-500 p-52 max-w-lg grid m-auto w-full">
         <div className="m-auto">
-          <h2 className="text-center text-gray-700 font-bold tracking-wider">Hello Word</h2>
+          <h2 className="text-center text-gray-700 font-bold">Hello Word</h2>
         </div>
       </div>
     </section>
