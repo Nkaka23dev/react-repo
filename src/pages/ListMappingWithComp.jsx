b0702a72e1af84362e1081b092065adfef1aa893
Nkaka23dev <nkakaeric96@gmail.com>
2023-02-22 15:05:45 +0200

    fix: applied change list on mappping with component

--- src/pages/ListMappingWithComp.jsx
+++ src/pages/ListMappingWithComp.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ListRenderCustomComp from '../components/ListRenderCustomComp'
 import { CustomListData } from '../data'
 
-CustomListData
 export default function ListMappingWithComp() {
   return (
     <ListRenderCustomComp data={CustomListData}/>
